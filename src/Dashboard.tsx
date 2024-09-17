113339173e72163a44ae182b59702439fa751273
John Alexander <alexander.john@gmail.com>
2024-09-16 23:58:09 -0700

    changing button color

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -16,8 +16,8 @@ export default function Dashboard({handleLogout}: DashboardProps) {
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-2xl font-bold">Dashboard</h1>
         <button
-          onClick={handleLogout}
-          className="p-2 bg-red-500 text-white rounded hover:bg-red-600"
+            onClick={handleLogout}
+            className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
         >
           Logout
         </button>
